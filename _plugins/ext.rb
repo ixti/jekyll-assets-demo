d991b60b81b3dc690641586702689e210d3ee89c
Alexey Zapparov <ixti@member.fsf.org>
2017-04-26 12:35:04 +0200

    Update Jekyll and Jekyll::Assets to latest versions
    Show how to re-enable ERB processor back.

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,2 +1,9 @@
 require "json"
 require "jekyll-assets"
+
+Jekyll::Assets::Hook.register :env, :init, :early do
+  # Re-enable ERB support
+  self.config = hash_reassoc(config, :engines) do |hash|
+    hash.merge(".erb" => ::Sprockets::ERBProcessor)
+  end
+end
